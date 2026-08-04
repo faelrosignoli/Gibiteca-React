923ae2ab6f26cf179e629ca2af0e6b31546a5cb3
faelrosignoli <rafaelorosignoli@gmail.com>
2026-08-04 09:37:53 -0300

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// base: './' => build funciona como site estático em qualquer pasta (GitHub Pages inclusive)
-export default defineConfig({
-  plugins: [react()],
-  base: './'
-})
